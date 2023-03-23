c81517308c7a7a8059e5e83c33cc6697660799a2
Alexandro <alexlmangabeira@gmail.com>
2023-03-23 12:18:14 -0300

    Trabalhando com imagens

--- avancando-react/src/App.js
+++ avancando-react/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import Paisagem from './assets/praia_02.jpg'
+
 function App() {
   return (
     <div className="App">
-      <h1>Avançando no React</h1>      
+      <h1>Avançando no React</h1>
+      {/* imagem em public */}
+      <h3>Imagem na diretorio public</h3>
+      <img src="/praia_01.jpg" alt="Praia" />
+      <div>
+        {/* imagem em assets */}
+        <h3>Imagem no diretorio assets</h3>
+        <img src={ Paisagem } alt="paisagem" />
+      </div>
     </div>
   );
 }
