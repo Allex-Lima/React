00f1ffd483a4a42fb7c7553c9ec8eb6836b726dc
Alexandro <alexlmangabeira@gmail.com>
2023-04-01 10:03:32 -0300

    Propriedade key

--- avancando-no-react/modulo_three/src/components/ListRender.js
+++ avancando-no-react/modulo_three/src/components/ListRender.js
@@ -1,13 +1,17 @@
 
 const ListRender = () => {
 
-    const nomes = ['Alex', 'Kátia', 'Bárbara', 'João']
+    const nomes = [
+        { id: 1234, nome: 'Alex', idade: 35 },
+        { id: 563453, nome: 'lex', idade: 45 },
+        {id: 4, nome: 'Ale', idade: 33}
+    ]
 
     return (
         <div>
             <ul>
-                {nomes.map((nome, indice) => (
-                    <li key={indice}>{ nome }</li>
+                {nomes.map(({nome, id}) => (
+                    <li key={id}>{ nome }</li>
                 ))}
             </ul>
         </div>
