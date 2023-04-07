34312ebfc16aaa22624fa3e70c25bb6555a6bf37
Alexandro <alexlmangabeira@gmail.com>
2023-04-06 21:10:53 -0300

    Operador ternário

--- avancando-no-react/modulo_three/src/components/ConditionalRender.js
+++ avancando-no-react/modulo_three/src/components/ConditionalRender.js
@@ -2,12 +2,17 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
 
-const [x] = useState(false)
+const [x, setX] = useState(true)
 
     return (
         <div>
             <h1>Isso será exibido?</h1>
             {x && <p>Se x for true, sim</p>}
+
+            <h1>Adicionando um else</h1>
+            <p>Operador ternário</p>
+            {x === true ? (<p>TRUE</p>) : (<p>FALSE</p>)} 
+            <button onClick={() => setX(!x)}>{x === true ? (<p>TRUE</p>) : (<p>FALSE</p>)}</button>
         </div>
     )
 }
