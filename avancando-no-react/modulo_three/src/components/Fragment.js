d70d0ab397dc4187b0c5ab6b73dc6384e5b2a0c8
Alexandro <alexlmangabeira@gmail.com>
2023-04-15 11:37:17 -0300

    Fragment

---
+++ avancando-no-react/modulo_three/src/components/Fragment.js
@@ -0,0 +1,11 @@
+const Fragment = ({ FragmentProps }) => {
+    return (
+        <>
+            <h1>Promeiro Título</h1>
+            <h2>Segundo Título</h2>
+            <h3>{ FragmentProps }</h3>
+        </>
+    )
+}
+
+export default Fragment
