f276dc956481f316e8b7438287879be9f66f15d3
Alexandro <alexlmangabeira@gmail.com>
2023-04-25 21:40:15 -0300

    CSS global | componentes

--- 4_css_react/css/src/App.js
+++ 4_css_react/css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/myComponent/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
+      {/* CSS no componente */}
+      <p>Este paragrafo não é do componente</p>
+      <MyComponent />
     </div>
   );
 }
---
+++ 4_css_react/css/src/components/myComponent/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>My Component</h1>
+            <p>Este é o paragrafo do componente</p>
+            <p className='my-comp-paragraph'>Este paragrafo também é do componente</p>
+        </div>
+    )
+}
+
+export default MyComponent
