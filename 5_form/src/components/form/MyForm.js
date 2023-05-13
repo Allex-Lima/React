f029f221b1363fb4fb8310dbeecc08a9b439e97a
Alexandro <alexlmangabeira@gmail.com>
2023-05-13 12:25:28 -0300

    gerenciamento de dados

--- 5_form/src/components/form/MyForm.js
+++ 5_form/src/components/form/MyForm.js
@@ -1,16 +1,31 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+    // gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        e.preventDefault()
+        setName(e.target.value);
+    }
+    console.log(name);
     return (
         <div>
             <h2>Formulário</h2>
-            {/* criação de form */}
+            {/* 1 - criação de form */}
             <form>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" placeholder="Digite seu nome"/>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Digite seu nome"
+                        onChange={handleName}
+                    />
                 </div>
-                {/* label envolvendo o input */}
+                {/* 2 - label envolvendo o input */}
                 <label>
                     <span>E-mail: </span>
                     <input type="email" name="email" placeholder="Digite seu e-mail"/>
